8297e54bc6736bd1da265c1418f7f54579cd6a8b
Mason Bell <masonbell812@gmail.com>
2026-03-11 12:55:26 +1000

    Initial commit

---
+++ ball.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var speed = 300.0
+var velocity = Vector2(250,250)
+var screen_size = Vector2.ZERO
+var direction = Vector2.ZERO
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	direction.x = -1
+	
+	position += direction * speed * delta
+
+
+func _on_player_paddle_area_entered(area: Area2D) -> void:
+	direction.x = 1
